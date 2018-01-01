616936b766e51f975ddb611d5cacb46842e1550c
Dunya Kirkali <dunyakirkali@gmail.com>
2018-01-01 14:57:58 +0100

    js ify

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,7 @@ class HomeController < ApplicationController
   before_action :set_methods
 
   def index
+    @title = "Random".split('').shuffle.join
     @random = "Faker::#{@fkr_module}".constantize.send(@fkr_method)
   end
 
